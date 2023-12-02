0de557ae7ef7df9396a9144811537b8019281c88
anshuravi <anshu.ravi2002@gmail.com>
2023-12-02 11:27:46 -0800

    update 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let currentPage = 1; // Initialize the current page
+
+function turnPage() {
+    const maxPages = 3; // Set the total number of pages
+
+    // Increment the current page or reset to 1 if it's the last page
+    currentPage = currentPage === maxPages ? 1 : currentPage + 1;
+
+    // Update the image source based on the current page
+    document.getElementById('medievalImage').src = `medieval_image${currentPage}.jpg`;
+}
